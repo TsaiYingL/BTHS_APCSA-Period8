08432bdf33c3b69e68323ef1f823d44168661f62
TsaiYing <tsaiying11100815@gmail.com>
2023-09-11 22:48:20 -0400

    1.2 Variables and Data Types

---
+++ Main1_2.java
@@ -0,0 +1,24 @@
+public class Main1_2
+{
+  public static void main(String[] args)
+  {
+    int integer; //creating an int and printing it out
+    integer = 10;
+    System.out.print("integer: ");
+    System.out.println(integer);
+   
+    double num; //creating a double and printint it
+    num = 0.5;
+    System.out.print("double: ");
+    System.out.println(num);
+   
+    boolean bool; //creating a boolean and printing it
+    bool = true;
+    System.out.println(bool);
+    bool = false;
+    System.out.println(bool);
+   
+    final double PI = 3.14; //creating a constant
+    System.out.print(PI);
+  }
+}
