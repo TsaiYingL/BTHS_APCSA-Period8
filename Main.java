a7f1601d0d421aed78a1818da821e5d7284b54f6
TsaiYing <tsaiying11100815@gmail.com>
2023-09-16 00:13:27 -0400

    finding area or prim of rec

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ public class Main
   public static void main(String[] args)
   {
     boolean loggedIn = false; //creating a boolean and printing it
-    String key = "s12345";
+    String key = "a";
+    boolean accountOpen = false;
    
     System.out.println("Hello! what is your name?");
     Scanner scan = new Scanner(System.in);
@@ -13,27 +14,44 @@ public class Main
     System.out.println("Hello " + name + "\n" + "what is your password?");
     String password = scan.nextLine();
     while (loggedIn==false) {
-      if (password instanceof String) {
-      System.out.println("true");
-      }
       if (password.equals(key)) {
         System.out.println("logged in!");
         loggedIn = true;
+        accountOpen = true;
+      } else if (password.equals("q")) {
+        System.out.println("exiting...");
+        return;
       } else {
-        System.out.println("wrong password!");
+        System.out.println("wrong password! Try again" + "\n" + "Type 'q' to quit");
         password = scan.nextLine();
       }
     }
-    scan.close();
    
-    int integer; //creating an int and printing it out
-    integer = 10;
-    System.out.println("integer: " + integer);
-   
-    double decimal = 0.5; //creating a double and printint it
-    System.out.println("double: " + decimal);
-   
-    final double PI = 3.14; //creating a constant
-    System.out.println("Pi: " + PI);
+    if (loggedIn==true) {
+      while (accountOpen ==true) {
+        System.out.println("Do you want to find the area (a) or the perimeter (p) of the rectangle? (q to exit)");
+        String operation= scan.nextLine();
+        if (operation.equals("q")) {
+              System.out.println("exiting...");
+              return;
+        } else if (operation.equals("a")||operation.equals("p")) {
+          try {
+            System.out.print("side 1: ");
+            double side1 = scan.nextInt();
+            System.out.print("side 2: ");
+            double side2 = scan.nextInt();
+            if (operation.equals("a")) {
+              System.out.println((side1*side2));
+            } else {
+              System.out.println(2*(side1+side2));
+            }
+          } catch (Exception e) {
+            System.out.println("Not a number");
+          }
+        }
+      }
+         
+    }
+   scan.close();
   }
 }
