c5dbd46b10de205c3a47c6211c1bed7b3a9c548a
TsaiYing <tsaiying11100815@gmail.com>
2023-09-20 00:20:27 -0400

    turned the code into a calculator

--- Main.java
+++ Main.java
@@ -13,13 +13,17 @@ public class Main
     String name = scan.nextLine();
     System.out.println("Hello " + name + "\n" + "what is your password?");
     String password = scan.nextLine();
-    while (loggedIn==false) {
-      if (password.equals(key)) {
+    while (loggedIn==false)
+    {
+      if (password.equals(key))
+      {
         System.out.println("logged in!");
         loggedIn = true;
         accountOpen = true;
+        scan.close();
       } else if (password.equals("q")) {
         System.out.println("exiting...");
+        scan.close();
         return;
       } else {
         System.out.println("wrong password! Try again" + "\n" + "Type 'q' to quit");
@@ -27,26 +31,34 @@ public class Main
       }
     }
    
-    if (loggedIn==true) {
-      while (accountOpen ==true) {
-        System.out.println("Do you want to find the area (a) or the perimeter (p) of the rectangle? (q to exit)");
-        String operation= scan.nextLine();
-        if (operation.equals("q")) {
+    if (loggedIn==true)
+    {
+      Scanner scan2 = new Scanner(System.in);
+      while (accountOpen ==true)
+      {
+        System.out.println("Do you want to find the product or the quotient(q to exit)?");
+        String operation= scan2.nextLine();
+        if (operation.equals("q"))
+        {
               System.out.println("exiting...");
               return;
-        } else if (operation.equals("a")||operation.equals("p")) {
-          try {
-            System.out.print("side 1: ");
-            double side1 = scan.nextInt();
-            System.out.print("side 2: ");
-            double side2 = scan.nextInt();
-            if (operation.equals("a")) {
-              System.out.println((side1*side2));
+        } else if (operation.equals("product")||operation.equals("quotient")) {
+          try
+          {
+            System.out.print("first num: ");
+            double num1 = scan2.nextDouble();
+            System.out.print("second num: ");
+            double num2 = scan2.nextDouble();
+            if (operation.equals("product")) {
+              System.out.println(num1*num2);
             } else {
-              System.out.println(2*(side1+side2));
+              System.out.println("result: " + num1/num2);
+              if (num1==(int)num1 && num2 == (int)num2) {
+              System.out.println("quotient: " + (int)(num1/num2) + " remainder: " + (int)(num1%num2));
+              }
             }
           } catch (Exception e) {
-            System.out.println("Not a number");
+            System.out.println(e);
           }
         }
       }
