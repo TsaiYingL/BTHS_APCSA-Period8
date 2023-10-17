f6875b5f73f39813d1ac8a53b8a526222090b3d7
TsaiYingL <117708691+TsaiYingL@users.noreply.github.com>
2023-10-17 14:06:35 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,3 +1,36 @@
-public class Game
+public class GameInfo
 {
+  private int randomNum;
+  private int numGuess;
+  private boolean isNumberCorrect = False;
+  
+  public void randomNum (int randomNum)
+  {
+    this.randomNum = randomNum;
+  }
+
+  public Int getRandomNum ()
+  {
+    return randomNum;
+  }
+  
+  public void numGuess (int numGuess)
+  {
+    this.numGuess = numGuess;
+  }
+
+  public Int getNumGuess ()
+  {
+    return numGuess;
+  }
+  
+  public void isNumCorrect (blooean isNumCorrect)
+  {
+    this.isNumCorrect = isNumCorrect;
+  }
+  
+  public blooean getResult()
+  {
+    return isNumCorrect;
+  }
 }
